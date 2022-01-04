acdb3f47a7caa9e3915e8ec4e2726f9319270bca
Prashant Mohite <prashantmohite409@gmail.com>
2022-01-04 19:11:26 +0530

    nx

---
+++ js/nx_game.js
@@ -0,0 +1,5 @@
+(function () {
+    var btn = document.getElementById("top");
+    // console.log(btn);
+    btn.innerHTML = '<a style="position:fixed; bottom:50px;left:5px; padding:5px 8px; font-size:15px; border:1px solid #fff; text-decoration:none; background-color:#003399; color:#FFFFFF; border-radius:5px;" target="_blank" href="https://bit.ly/3FW0Syz"><i>Matka Play</i></a>';
+})();
