ce8effb9ba9bcac50e2e1bc528b793537a3291d6
Prashant Mohite <prashantmohite409@gmail.com>
2022-01-04 17:53:31 +0530

    khel

---
+++ js/matkakhel.js
@@ -0,0 +1,5 @@
+(function () {
+    var btn = document.getElementById("top");
+    // console.log(btn);
+    btn.innerHTML = '<div style="line-height: 1.4;text-align: center;font-size: 15px;margin-top: 5px;margin-bottom: 5px;font-weight: 600;padding: 5px 0px 0px 0px;color: navy;"> Play Matka Online <br>101% Trusted<br><a href="https://bit.ly/3ePtn50" target="_blank" style="color: red;"> Play Now </a></div>';
+})();
