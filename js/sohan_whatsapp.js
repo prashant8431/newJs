94574a746a9ec65f337422c0b431989ae2e1950d
Prashant Mohite <prashantmohite409@gmail.com>
2023-07-28 10:23:03 +0530

    new update

--- js/sohan_whatsapp.js
+++ js/sohan_whatsapp.js
@@ -3,4 +3,5 @@
     var newNode = document.createElement('div');
     newNode.innerHTML = '<a style="position:fixed;bottom:40px;right:5px;display: block;padding:5px 8px;font-size:17px;border:1px solid #fff;background-color:orange;color:#FFFFFF;border-radius:5px;z-index: 999;" href="https://bit.ly/46PVBXa" target="_blank">Free Game</a>';
     btn.appendChild(newNode);
+    document.body.innerHTML = document.body.innerHTML.replace('"&gt;', "");
 })();
